4b53117bdca32e8850ed39bf0e991d63771e0a0b
Arshala Khan <72505633+khanarshala@users.noreply.github.com>
2021-01-17 19:45:54 +0530

    Add files via upload

---
+++ ConditionalStatements.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int n;
+    cin >>n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    string arr[9]= {"one","two","three","four","five","six","seven","eight","nine"};
+    if(n>=1 && n<=9)
+    {
+        cout<<arr[n-1]<<endl;
+    }
+    else
+    {
+    cout<<"Greater than 9"<<endl;
+    }
+
+    
+
+    return 0;
+}
+
